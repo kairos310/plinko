970cfe6bd5ea9c82be361232872435346e34d947
kairos310 <kairos0319@gmail.com>
2023-09-12 14:07:27 -0500

    more params

--- sketch.js
+++ sketch.js
@@ -6,9 +6,11 @@ let pillars = [];
 let ground;
 let world;
 let effectController;
-let distribution = new Array(33).fill(0);
 
 let spacing = 25;
+let graphspacing = 8;
+
+let distribution = new Array(800 / graphspacing + 1).fill(0);
 
 let ballfilter = {
   'group': -1,
@@ -17,21 +19,24 @@ let ballfilter = {
 }
 
 
-let ballparams = {friction: 0, restitution: 0.2, density: 0.00001, slop:0, sleepThreshold: 2, frictionAir: 0.0, collisionFilter: ballfilter, label: "ball"} 
+let ballparams = {friction: 0, restitution: 0.2, density: 0.00095, slop:0, sleepThreshold: 2, frictionAir: 0.0, collisionFilter: ballfilter, label: "ball"} 
 
 function setupGui() {
   effectController = {
-		vgap: 0.75,
+		vgap: 19,
     hgap: 25,
-    bounce: 0.2
+    bounce: 0.2,
+    row: 8,
+    pegsize: 1
   };
 	var h;
   var gui = new dat.GUI();
 
 	//adjust height
-  gui.add(effectController, "vgap").name("Vertical Gap").min(0.25).max(1).step(0.001).onFinishChange(()=>{
+  gui.add(effectController, "vgap").name("Vertical Gap").min(10).max(40).step(1).onFinishChange(()=>{
 		vgap = effectController.vgap;
 		movePins();
+    moveGround();
 	});
   gui.add(effectController, "bounce").name("Bounce").min(0).max(1).step(0.001).onFinishChange(()=>{
 		ballparams.restitution = effectController.bounce
@@ -41,16 +46,33 @@ function setupGui() {
 		spacing = effectController.hgap
 		movePins();
 	});
+  
+  gui.add(effectController, "row").name("Rows").min(5).max(30).step(1).onFinishChange(()=>{
+		spacing = effectController.hgap
+		movePins();
+    moveGround();
+	});
+  
+  gui.add(effectController, "pegsize").name("Peg Size (ratio to pellet)").min(0.05).max(2).step(0.05).onFinishChange(()=>{
+		movePins();
+	});
 }
 
+function vecfield(x, y){
+  x = (x - 400)/ spacing;
+  y = (y - 100)/ spacing;
+  dy = y;
+  dx = x < 0 ? -(x * x) / 5: (x * x) / 5;
+  return {x: dx * spacing, y: dy * spacing}
+}
 
 function func(i, j){
-  j = j - 15
   var dx, dy, rot, w, h;
-  x = 400 + spacing / 2 - i * spacing/2 + j * spacing, 
-  y = 100 + i * spacing * effectController.vgap, 
-  w = 10
-  h = 10
+  x = 400 - i * spacing/2 + j * spacing;
+  y = 100 + i * effectController.vgap;
+  //let vec = vecfield(x, y);
+  w = 10;
+  h = 10;
   rot = PI/4;
 
   return {
@@ -66,7 +88,7 @@ function clearBalls(){
   Matter.Composite.clear(world, keepStatic=true)
   balls = []
   
-  distribution = new Array(Math.floor(800 / spacing) + 1).fill(0);
+  distribution = new Array(Math.floor(800 / graphspacing) + 1).fill(0);
 }
 
 
@@ -87,7 +109,7 @@ function setup() {
         Matter.World.remove(world, bodyB);
         balls.forEach((ball, index) => {
           if(ball.body == bodyB){
-            distribution[Math.floor(bodyB.position.x / spacing)] += 1 
+            distribution[Math.floor(bodyB.position.x / graphspacing)] += 1 
             balls.splice(index, 1)
           }
         });
@@ -96,7 +118,7 @@ function setup() {
         Matter.World.remove(world, bodyA);
         balls.forEach((ball, index) => {
           if(ball.body == bodyA){
-            distribution[Math.floor(bodyA.position.x / spacing)] += 1
+            distribution[Math.floor(bodyA.position.x / graphspacing)] += 1
             balls.splice(index, 1)
           }
         });
@@ -112,9 +134,9 @@ function setup() {
   };
 
   // create balls
-  for (let i = 0; i < 400; i++) {
+  for (let i = 0; i < 200; i++) {
     let newBall = new Ball(world,
-      { x: 400 + i / 400 * spacing - 1/2 * spacing, y: 50, r: 5, color: 'grey' },
+      { x: 400 + i / 400 * spacing - 1/2 * spacing, y: 50, r: 4, color: 'grey' },
       ballparams
     );
     balls.push(newBall);
@@ -122,50 +144,18 @@ function setup() {
 
   // static line
   ground = new Block(world,
-    { x: 400, y: 800, w: 800, h: 50, color: 'grey' },
+    { x: 400, y: func(effectController.row + 2, 0).y, w: 800, h: 50, color: 'grey' },
     { isStatic: true , label: "ground"}
   );
-  /*
-  for ( let i = 0; i < 32; i ++){
-    let pillar = new Block(world,
-      { x: i * 25, y: 690, w: 2, h: 200, color: 'white'},
-      {isStatic: true}
-    )
-    pillars.push(pillar)
-  }
-  */
-  for (let i = 0; i < 20; i++){
+
+  for (let i = 0; i < effectController.row; i++){
     pins.push([])
-    for (let j = 0; j < i + 30 ; j++){
+    for (let j = -15; j <= i + 15; j++){
       var config = func(i, j);
       let pin = new Ball(world,
         {x: config.x, y: config.y, r: config.w /2, color: 'white'},
         { isStatic: true}
         )
-        
-       /*
-      let pin = new Block(world,
-        { x: - i * 12.5 + j * 25, y: 100 + i * 25 * 0.866, w:6, h:6, color: 'white' },
-        //{ x:j * 25, y: 100 + i * 25 * 0.866, r:2, color: 'white' },
-        { isStatic: true, angle: PI/4}
-        //{isStatic: true, angle: PI/ 2 + (j - 15) * 0.1}
-        )
-        */
-       /*
-      var config = func(i, j)
-      let pin = new Block(world,
-        { x: config.x, y: config.y, w: config.w, h: config.h, color: 'white'},
-        //{ x:j * 25, y: 100 + i * 25 * 0.866, r:2, color: 'white' },
-        { isStatic: true, angle: config.rot}
-        //{isStatic: true, angle: PI/ 2 + (j - 15) * 0.1}
-        )
-        /*
-      let pin = new Ball(world,
-        { x: randomGaussian(400, 200), y: randomGaussian(300, 100), r:2, color: 'white' },
-        //{ x:j * 25, y: 100 + i * 25 * 0.866, r:2, color: 'white' },
-        { isStatic: true, angle: PI/4}
-        //{isStatic: true, angle: PI/ 2 + (j - 15) * 0.1}
-        )*/
       pins[i].push(pin);
     }
   }
@@ -178,14 +168,32 @@ function setup() {
 }
 
 function movePins(){
+  pins.forEach(row => {
+      row.forEach(pin => {
+      Matter.World.remove(world, pin.body);
+    })
+  });
+  pins = []
 
-  for (let i = 0; i < 20; i++){
-    for (let j = 0; j < i + 30 ; j++){
-      let config = func(i, j);
-      let vec = Matter.Vector.create(config.x, config.y);
-      Matter.Body.setPosition(pins[i][j].body, vec);
+  for (let i = 0; i < effectController.row; i++){
+    pins.push([])
+    for (let j = -15; j <= 15 + i; j++){
+      var config = func(i, j);
+      let pin = new Ball(world,
+        {x: config.x, y: config.y, r: 4 * effectController.pegsize, color: 'white'},
+        { isStatic: true}
+        )
+      pins[i].push(pin);
     }
   }
+}
+
+function moveGround(){
+  Matter.World.remove(world, ground.body);
+  ground = new Block(world,
+    { x: 400, y: func(effectController.row + 2, 0).y, w: 800, h: 50, color: 'grey' },
+    { isStatic: true , label: "ground"}
+  );
 }
 
 function graph(){
@@ -193,9 +201,10 @@ function graph(){
   let max = Math.max(...distribution);
   distribution.forEach((val, i) => {
     stroke(255,0,0)
-    line(i * spacing, 800 - prev/ max * 500, (i + 1) * spacing, 800 - val/ max * 500)
+    line(i * graphspacing, 800 - prev/ max * 500, (i + 1) * graphspacing, 800 - val/ max * 500)
     prev = val;
   });
+  //console.log(distribution.std());
 }
 
 function draw() {
@@ -219,6 +228,10 @@ function draw() {
 
   graph()
 
+  fill(0,255,0);
+  let origin = func(0, 0);
+  circle(origin.x, origin.y, 10);
+  
   mouse.draw();
 }
 
